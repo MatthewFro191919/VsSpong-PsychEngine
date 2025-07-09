faf2c7bdcab73827656174d3f3acaa5c76166d3f
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-09 19:28:15 -0400

    Update SpongStage.hx

--- source/states/stages/SpongStage.hx
+++ source/states/stages/SpongStage.hx
@@ -6,6 +6,8 @@ import cutscenes.DialogueBox;
 
 class SpongStage extends BaseStage
 {
+	public var dialogue:Array<String> = ['dad:blah blah blah', 'bf:coolswag'];
+
 	override function create()
 	{
 		//dialogue shit
@@ -43,66 +45,7 @@ class SpongStage extends BaseStage
 			add(stageCurtains);
 		}
 	}
-	override function eventPushed(event:objects.Note.EventNote)
-	{
-		switch(event.event)
-		{
-			case "Dadbattle Spotlight":
-				dadbattleBlack = new BGSprite(null, -800, -400, 0, 0);
-				dadbattleBlack.makeGraphic(Std.int(FlxG.width * 2), Std.int(FlxG.height * 2), FlxColor.BLACK);
-				dadbattleBlack.alpha = 0.25;
-				dadbattleBlack.visible = false;
-				add(dadbattleBlack);
 
-				dadbattleLight = new BGSprite('spotlight', 400, -400);
-				dadbattleLight.alpha = 0.375;
-				dadbattleLight.blend = ADD;
-				dadbattleLight.visible = false;
-				add(dadbattleLight);
-
-				dadbattleFog = new DadBattleFog();
-				dadbattleFog.visible = false;
-				add(dadbattleFog);
-		}
-	}
-
-	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float)
-	{
-		switch(eventName)
-		{
-			case "Dadbattle Spotlight":
-				if(flValue1 == null) flValue1 = 0;
-				var val:Int = Math.round(flValue1);
-
-				switch(val)
-				{
-					case 1, 2, 3: //enable and target dad
-						if(val == 1) //enable
-						{
-							dadbattleBlack.visible = true;
-							dadbattleLight.visible = true;
-							dadbattleFog.visible = true;
-							defaultCamZoom += 0.12;
-						}
-
-						var who:Character = dad;
-						if(val > 2) who = boyfriend;
-						//2 only targets dad
-						dadbattleLight.alpha = 0;
-						new FlxTimer().start(0.12, function(tmr:FlxTimer) {
-							dadbattleLight.alpha = 0.375;
-						});
-						dadbattleLight.setPosition(who.getGraphicMidpoint().x - dadbattleLight.width / 2, who.y + who.height - dadbattleLight.height + 50);
-						FlxTween.tween(dadbattleFog, {alpha: 0.7}, 1.5, {ease: FlxEase.quadInOut});
-
-					default:
-						dadbattleBlack.visible = false;
-						dadbattleLight.visible = false;
-						defaultCamZoom -= 0.12;
-						FlxTween.tween(dadbattleFog, {alpha: 0}, 0.7, {onComplete: function(twn:FlxTween) dadbattleFog.visible = false});
-				}
-		}
-	}
 	function schoolIntro(?dialogueBox:DialogueBox):Void
 	{
 		inCutscene = true;
@@ -125,11 +68,11 @@ class SpongStage extends BaseStage
 
 		senpaiEvil.screenCenter();
 
-		if (songLowercase == 'abrasive' || songLowercase == 'pineappled')
+		if (songName == 'abrasive' || songName == 'pineappled')
 		{
 			remove(black);
 
-			if (songLowercase == 'pineappled')
+			if (songName == 'pineappled')
 			{
 				add(red);
 			}
@@ -149,7 +92,7 @@ class SpongStage extends BaseStage
 				{
 					inCutscene = true;
 
-					if (songLowercase == 'pineappled')
+					if (songName == 'pineappled')
 					{
 						add(senpaiEvil);
 						senpaiEvil.alpha = 0;
